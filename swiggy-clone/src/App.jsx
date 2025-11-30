915974943f73ba53326385e0b97cba18f86009a0
Palaklulla_08 <palaklulla54@gmail.com>
2025-11-30 15:19:14 +0530

    made the whats in your mind body element

--- swiggy-clone/src/App.jsx
+++ swiggy-clone/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Navbar from './components/Navbar'
+import Body from './components/Body'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Body/>
     </>
   )
 }
